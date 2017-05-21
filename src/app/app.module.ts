f96b24773019be3633f2eab6b76e9a940bdb51f4
Riley Du <riley.du@gmail.com>
2017-05-21 15:43:30 -0700

    create structure of webpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserComponent } from './user/user.component';
+import { JoblistComponent } from './joblist/joblist.component';
+import { JobComponent } from './joblist/job/job.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    UserComponent,
+    JoblistComponent,
+    JobComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
